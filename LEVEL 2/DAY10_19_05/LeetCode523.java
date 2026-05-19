354c89194fbf38f7e3bf21f4407f552932e6a7f1
Samriddhi Kapoor <samriddhi2706@gmail.com>
2026-05-19 11:54:05 +0530

    Implement checkSubarraySum method in Solution class

---
+++ LEVEL 2/DAY10_19_05/LeetCode523.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int prefixSum=0;
+        for(int i=0;i<nums.length;i++){
+            prefixSum=(prefixSum+nums[i])%k;
+            if(map.containsKey(prefixSum)){
+                if(i-map.get(prefixSum)>=2) return true;
+            }else{
+                map.put(prefixSum,i);
+            }
+        }
+        return false;
+    }
+}
