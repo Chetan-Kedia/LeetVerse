7f0bebae1649a2c15daa74c27432d5db2ef1293a
Samriddhi Kapoor <samriddhi2706@gmail.com>
2026-05-26 12:31:47 +0530

    Implement findDuplicates method in LeetCode442

---
+++ LEVEL 2/DAY15_25_05/LeetCode442.java
@@ -0,0 +1,15 @@
+class Solution {
+   public List<Integer> findDuplicates(int[] nums) {
+     int n = nums.length;
+     List<Integer> res = new ArrayList<>();
+     for (int i = 0; i < n; i++) {
+       int idx = Math.abs(nums[i]) - 1;
+       if (nums[idx] < 0) {
+         res.add(Math.abs(nums[i]));
+       } else {
+         nums[idx] *= -1;
+       }
+     }
+     return res;
+   }
+}
