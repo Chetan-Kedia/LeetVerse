855e30bcd753999d35a2afdfef1cad18d308e264
Samriddhi Kapoor <samriddhi2706@gmail.com>
2026-05-29 17:51:12 +0530

    Add solution for Happy Number problem

---
+++ LEVEL 2/DAY18_28_05/LeetCode202.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumOfDigits(slow);
+            fast = sumOfDigits(sumOfDigits(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int sumOfDigits(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += (digit * digit);
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
