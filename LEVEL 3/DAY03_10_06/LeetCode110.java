ceb5fba2eb35393ee731f182765ebb52ec56ccdd
Samriddhi Kapoor <samriddhi2706@gmail.com>
2026-06-11 23:11:27 +0530

    Implement balanced tree check in LeetCode110

---
+++ LEVEL 3/DAY03_10_06/LeetCode110.java
@@ -0,0 +1,18 @@
+class Solution {
+  public boolean isBalanced(TreeNode root) {
+    return checkHeight(root) != -1;
+  }
+
+  private int checkHeight(TreeNode node) {
+    if (node == null) return 0;
+
+    int leftH = checkHeight(node.left);
+    if (leftH == -1) return -1;
+
+    int rightH = checkHeight(node.right);
+    if (rightH == -1) return -1;
+
+    if (Math.abs(leftH - rightH) > 1) return -1;
+    return Math.max(leftH, rightH) + 1;
+  }
+}
